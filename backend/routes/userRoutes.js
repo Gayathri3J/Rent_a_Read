243d73dc6a0afac942c50fc7b701bbd272b52b88
Gayathri J <gayathrij263@gmail.com>
2025-10-05 23:28:15 +0530

    4th commit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,17 +1,15 @@
 import express from 'express';
-import { registerUser, authUser, getUsers, getUserById, forgotPassword, resetPassword, testEmail } from '../controllers/userController.js';
+import { registerUser, authUser, getUsers, getUserById, testEmail } from '../controllers/userController.js';
 import { protect, admin } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/login', authUser);
-router.post('/forgot-password', forgotPassword);
-router.post('/reset-password', resetPassword);
 router.post('/test-email', testEmail);
 
 
-router.get(['/login', '/forgot-password', '/reset-password', '/test-email'], (req, res) => {
+router.get(['/login', '/test-email'], (req, res) => {
   res.status(405); 
   throw new Error(`GET method is not supported for ${req.path}. Please use POST.`);
 });
